5d406201c224768efb9e62e526c80a2cb3f5d4c1
goldenkaia <julia_tserkovna@gmx.net>
2023-07-01 17:33:33 +0200

    start commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const menuBtnEl = document.querySelector(".js-btn-menu");
+const menuBoxEl = document.querySelector(".js-box-menu");
+
+menuBtnEl.addEventListener("click", onClickMenuBtn);
+
+function onClickMenuBtn() {
+  menuBoxEl.classList.toggle("show");
+}
